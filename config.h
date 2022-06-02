346aee75021eee4ab5db67465e7e29e3e733b84a
Lorenzo Calza <lorenzo@lorenzocalza.com>
2022-06-03 00:20:00 +0200

    Change font to SF Mono

--- config.h
+++ config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=38:antialias=true:autohint=true";
+//static char *font = "Liberation Mono:pixelsize=38:antialias=true:autohint=true";
+static char *font = "SF Mono:style=Light:pixelsize=38:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
